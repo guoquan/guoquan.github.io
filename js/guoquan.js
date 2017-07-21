2f2d963f0aca02847c8ae6c5eb57510ea3e91ab9
Guo Quan <guoquanscu@gmail.com>
2017-07-21 14:56:34 +0800

    Update publication list and some display style issues. Remove redundent js codes.

--- js/guoquan.js
+++ js/guoquan.js
@@ -1,31 +1,15 @@
 $(function(){
     "use strict";
 
-    $(window).on("ready, scroll", function(){
-        if ($(window).scrollTop() > 30) {
-            $('.main-menu').addClass('minified');
-        } else {
-            $('.main-menu').removeClass('minified');
-        }
-    });
-
-    $('.sidebar').affix({
-        offset: {
-            top: 100,
-            bottom: function () {
-                return (this.bottom = $('.footer').outerHeight(true))
-            }
-        }
-    });
-
     var b = $(".sidebar nav");
+    var e = 10; // .sidebar nav.affix { top: 10px }
     b.affix({
         offset: {
             top: function() {
                 var c = b.offset().top,
-                    d = parseInt(b.children(0).css("margin-top"), 10),
-                    e = $("header").height();
-                return this.top = c - e - d
+                    d = parseInt(b.css("margin-top"), 10),
+                    e = 20; //$("header").height();
+                return this.top = c - d - e
             },
             bottom: function() {
                 return this.bottom = $("footer").outerHeight(!0)
@@ -36,13 +20,6 @@ $(function(){
     $("body").scrollspy({
             target: ".sidebar"
     });
-    /*
-    $('.nav').onePageNav({
-        currentClass: 'active',
-        scrollSpeed: 500,
-        easing: 'linear'
-    });
-    */
 
     $.scrollUp({
         animation: 'slide',
